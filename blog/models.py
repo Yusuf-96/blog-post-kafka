d877e297d1190e12f5e527360d6b354b7a6c2e71
Yusuph Omari <yousufummar649@gmail.com>
2024-01-29 15:24:55 +0300

    initial commit and project setup

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+
+class Post(models.Model):
+    post_id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializers
+from rest_framework import status
+
+
+class CreatePostAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializers(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "status": status.HTTP_201_CREATED,
+                "message": "Post has been created sucessfully",
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+
+        response = {
+            "status": status.HTTP_400_BAD_REQUEST,
+            "message": "You did not post yet",
+        }
+
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
